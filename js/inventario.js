80c0dc95f5358a4fa18bfc29cf3aa5881f03cd5d
Andres Duran <aduran@tiresia.co>
2025-04-13 23:07:53 -0500

    first commit frontend

---
+++ js/inventario.js
@@ -0,0 +1,39 @@
+import { obtainCategories } from "./../apiConnection/consumeApi.js";
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    getCategories();
+});
+
+async function getCategories() {
+    const categoriasObtenidas = await obtainCategories();
+    const container = document.querySelector('tbody');
+
+    categoriasObtenidas.forEach((categoria) => {
+        const { CategoriaID, CategoriaNombre, Descripcion, Imagen } = categoria;
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>
+                ${CategoriaID}
+            </td>
+            <td>
+                ${CategoriaNombre}
+            </td>
+            <td>
+                ${Descripcion}
+            </td>
+            <td>
+                <img src"${Imagen}" width="100px" class="cuenta">
+            </td>
+            <td>
+            <button class="btn color3" >Details</button>
+            </td>
+            <td>
+            <button class="btn color2" >Edit</button>
+            </td>
+            <td>
+            <button class="btn color" >Delete</button>
+            </td>
+        `;
+        container.appendChild(row);
+    });
+}
